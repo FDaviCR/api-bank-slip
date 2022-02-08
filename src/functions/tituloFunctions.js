b0759260c0e1821f81239964f6c14ffaee323b23
Davi <davicamiloribeiro@gmail.com>
2022-02-07 22:28:22 -0300

    Função de código de barras refatorada

--- src/functions/tituloFunctions.js
+++ src/functions/tituloFunctions.js
@@ -2,10 +2,21 @@ const utils = require('../utils');
 
 module.exports = {
     getCodigoDeBarras(linhaDigitavel) {
-        return utils.retornaCodigoDeBarrasTitulo(linhaDigitavel);
+        let codigoBarras = '';
+
+        codigoBarras += linhaDigitavel.substring(0, 3);
+        codigoBarras += linhaDigitavel.substring(3, 4);
+        codigoBarras += linhaDigitavel.substring(32, 33);
+        codigoBarras += linhaDigitavel.substring(33, 37);
+        codigoBarras += linhaDigitavel.substring(37, 47);
+        codigoBarras += linhaDigitavel.substring(4, 9);
+        codigoBarras += linhaDigitavel.substring(10, 20);
+        codigoBarras += linhaDigitavel.substring(21, 31);
+
+        return codigoBarras;
     },
     getVencimento(linhaDigitavel){
-        const codigoBarras = utils.retornaCodigoDeBarrasTitulo(linhaDigitavel);
+        const codigoBarras = this.getCodigoDeBarras(linhaDigitavel);
         const dias = parseInt(codigoBarras.substring(5,9));
         
         const data = new Date(1997, 9, 7);
@@ -14,7 +25,7 @@ module.exports = {
         return JSON.stringify(data).substring(1, 11);
     },
     getValor(linhaDigitavel){
-        const codigoBarras = utils.retornaCodigoDeBarrasTitulo(linhaDigitavel);
+        const codigoBarras = this.getCodigoDeBarras(linhaDigitavel);
         let valor = codigoBarras.substring(9,19);
         
         return utils.formatValor(valor);;
--- src/utils/index.js
+++ src/utils/index.js
@@ -15,18 +15,4 @@ module.exports = {
         return value.replace(/( |\.|-)/g, '');
     },
 
-    retornaCodigoDeBarrasTitulo(value){
-        let codigoBarras = '';
-
-        codigoBarras += value.substring(0, 3);
-        codigoBarras += value.substring(3, 4);
-        codigoBarras += value.substring(32, 33);
-        codigoBarras += value.substring(33, 37);
-        codigoBarras += value.substring(37, 47);
-        codigoBarras += value.substring(4, 9);
-        codigoBarras += value.substring(10, 20);
-        codigoBarras += value.substring(21, 31);
-
-        return codigoBarras;
-    }
 }
